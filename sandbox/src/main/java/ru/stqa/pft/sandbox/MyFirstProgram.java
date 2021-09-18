c48a30c8c9a7418a32bb25e2a17d361f0e0e745b
Yakov Shvidler <yakov112798@gmail.com>
2021-09-18 22:36:59 +0300

    Reformated the code to look nicer

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-  public static void main(String[] args) {
- // generate some simple output
- System.out.println("Hello, World!");
- System.out.println("How are ya?"); //hello jacob
- }
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!");
+        System.out.println("How are ya?"); //hello jacob
+    }
 }
