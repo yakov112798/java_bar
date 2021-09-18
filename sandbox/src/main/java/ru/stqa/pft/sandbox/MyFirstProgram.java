2e0eb5ede026a4248338b22757292b33a4bbdb0f
Yakov Shvidler <yakov112798@gmail.com>
2021-09-18 21:14:37 +0300

    Created a package for "MyFirstProgram"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class Hello{ 
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
   public static void main(String[] args) {
  // generate some simple output
  System.out.println("Hello, World!");
