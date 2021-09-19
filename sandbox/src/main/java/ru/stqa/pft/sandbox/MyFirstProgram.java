cf94f1a2ea514cfcc630113490767095a6f5b922
Yakov Shvidler <yakov112798@gmail.com>
2021-09-19 11:29:00 +0300

    Created area calculating functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        // generate some simple output
-        System.out.println("Hello, World!");
-        System.out.println("How are ya?"); //hello jacob
+        hello("World");
+        hello("Jacob");
+        hello("User");
+
+        double i = 5.0;
+        System.out.println("The area of the square with length " + i + " = "+ area(i));
+
+        double x= 6;
+        double y=8;
+        System.out.println("The area of rectangle with length " + x + " and width " + y +" = "+area(x,y));
+
+    }
+    public static void hello(String sombebody) {
+        System.out.println("Hello " + sombebody);
+
+    }
+    public static double area(double x) {
+        return x*x;
+    }
+    public static double area(double x , double y){
+        return x*y;
     }
 }
