737db90e801633f11d5b6a994d923284c5d2933e
Yakov Shvidler <yakov112798@gmail.com>
2021-09-19 13:04:38 +0300

    Created Objects that take parameter for calculating area

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,26 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
+        Square s = new Square(5.0);
+        Rectangle r = new Rectangle(6.0,5.0);
         hello("World");
         hello("Jacob");
         hello("User");
 
-        double i = 5.0;
-        System.out.println("The area of the square with length " + i + " = "+ area(i));
+        System.out.println("The area of the square with length " + s.l + " = "+ area(s));
 
-        double x= 6;
-        double y=8;
-        System.out.println("The area of rectangle with length " + x + " and width " + y +" = "+area(x,y));
+
+        System.out.println("The area of rectangle with length " + r.a + " and width " + r.b +" = "+area(r));
 
     }
     public static void hello(String sombebody) {
         System.out.println("Hello " + sombebody);
 
     }
-    public static double area(double x) {
-        return x*x;
+    public static double area(Square s) {
+        return s.l*s.l;
     }
-    public static double area(double x , double y){
-        return x*y;
+    public static double area(Rectangle r){
+        return r.a*r.b;
     }
 }
