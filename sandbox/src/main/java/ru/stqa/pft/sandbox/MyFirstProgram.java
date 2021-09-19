17d1fc3e876ab588df395323e2c3182e374bb7e4
Yakov Shvidler <yakov112798@gmail.com>
2021-09-19 13:56:38 +0300

    Created Methods for calculating area inside the Rectangle and Square class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,20 +8,13 @@ public class MyFirstProgram {
         hello("Jacob");
         hello("User");
 
-        System.out.println("The area of the square with length " + s.l + " = "+ area(s));
+        System.out.println("The area of the square with length " + s.l + " = "+ s.area());
 
 
-        System.out.println("The area of rectangle with length " + r.a + " and width " + r.b +" = "+area(r));
+        System.out.println("The area of rectangle with length " + r.a + " and width " + r.b +" = "+r.area());
 
     }
     public static void hello(String sombebody) {
         System.out.println("Hello " + sombebody);
-
-    }
-    public static double area(Square s) {
-        return s.l*s.l;
-    }
-    public static double area(Rectangle r){
-        return r.a*r.b;
-    }
+  }
 }
